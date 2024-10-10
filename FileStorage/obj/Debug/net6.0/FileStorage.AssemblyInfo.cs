99e7a78839ca66b9da908db539bd9af1b87ed348
CamielAdriaens <camillothee@gmail.com>
2024-10-10 16:26:50 +0200

    bugfix merge problem

--- FileStorage/obj/Debug/net6.0/FileStorage.AssemblyInfo.cs
+++ FileStorage/obj/Debug/net6.0/FileStorage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69312cebe51ad281acd17d977ee2d8fd474129fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49619553f100f157b0ef1187293b17ad6b2f0307")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileStorage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
